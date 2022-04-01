3a85ca72815537506bf141d009aeba2c8fc36b2a
Aditya <aditya050995@gmail.com>
2022-03-31 17:08:41 -0700

    Minor cleanup
    Signed-off-by: Aditya <aditya050995@gmail.com>

--- total_mass.cpp
+++ total_mass.cpp
@@ -1,64 +1,29 @@
-#include <cstring>
 #include <iostream>
-#include <memory>
-#include <string>
 
-#include "sdf/sdf_config.h"
-#include "sdf/Filesystem.hh"
 #include "sdf/Root.hh"
-#include "sdf/parser.hh"
-#include "sdf/PrintConfig.hh"
-#include "sdf/system_util.hh"
 #include "sdf/Link.hh"
+#include "sdf/Model.hh"
 
 #include "ignition/math/Inertial.hh"
 
 int main(int argc, char *argv[]) {
- // check arguments
-  if (argc < 2)
-  {
-    std::cerr << "Usage: " << argv[0]
-              << " <sdf-path>" << std::endl;
+
+  sdf::Root root;
+  if (!root.Load(argv[1]).empty()) {
     return -1;
   }
-  const std::string sdfPath(argv[1]);
 
-  // load and check sdf file
-  sdf::SDFPtr sdfElement(new sdf::SDF());
-  sdf::init(sdfElement);
-  if (!sdf::readFile(sdfPath, sdfElement))
-  {
-    std::cerr << sdfPath << " is not a valid SDF file!" << std::endl;
-    return -2;
+  const sdf::Model * model = root.Model();
+  if (!model){ 
+    return -1;
   }
-
-  // start parsing model
-  const sdf::ElementPtr rootElement = sdfElement->Root();
-  if (!rootElement->HasElement("model"))
-  {
-    std::cerr << sdfPath << " is not a model SDF file!" << std::endl;
-    return -3;
-  }
-  const sdf::ElementPtr modelElement = rootElement->GetElement("model");
-  const std::string modelName = modelElement->Get<std::string>("name");
-  std::cout << "Found " << modelName << " model!\n" << std::endl;
 
   auto totalInertial = ignition::math::Inertiald();
-
-  // parse model links
-  sdf::ElementPtr linkElement = modelElement->GetElement("link");
-  while (linkElement)
-  {
-    const std::string linkName = linkElement->Get<std::string>("name");
-
-    sdf::Link current_link;
-    current_link.Load(linkElement);
-    totalInertial += current_link.Inertial();
-
-    linkElement = linkElement->GetNextElement("link");
+  for (int i = 0; i < model->LinkCount(); i++) {
+    totalInertial += model->LinkByIndex(i)->Inertial();  
   }
 
-  std::cout << "Total mass: "  << totalInertial.MassMatrix().Mass() << std::endl;
+  std::cout << "Total mass: " << totalInertial.MassMatrix().Mass() << std::endl;
 
   return 0;
 }
